12ab8f819f0898203f16af0e7cf557d76bd5b0b0
Nandani Chandoriya <nandanichandoriya79877@gmail.com>
2026-09-04 14:35:34 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered = "";
+         for(char c: s){
+            if(isalnum(c)){
+                filtered += tolower(c);
+            }
+         }
+        
+        string s_inv = filtered;
+         reverse(s_inv.begin(),s_inv.end());
+
+        if(filtered==s_inv){
+            return true;
+        }
+
+        return false;
+    }
+};
