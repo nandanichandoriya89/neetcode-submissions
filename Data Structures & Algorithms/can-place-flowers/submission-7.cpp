b75409c40d56b646da1527b0482b8dee79addabe
Nandani Chandoriya <nandanichandoriya79877@gmail.com>
2026-09-11 10:18:50 +0530

    Add: can-place-flowers - submission-7

---
+++ Data Structures & Algorithms/can-place-flowers/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if(flowerbed.size()==1){
+            if(flowerbed[0]==0){
+                return true;
+            }
+        }
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            flowerbed[0]=1;
+            n--;
+        }
+        if(flowerbed[flowerbed.size()-1]==0 && flowerbed[flowerbed.size()-2]==0){
+            flowerbed[flowerbed.size()-1]=1;
+            n--;
+        }
+        for (int i=1; i<flowerbed.size(); i++){
+                        if(flowerbed[i-1]==0 && flowerbed[i+1]==0 && flowerbed[i]==0){
+                                        flowerbed[i]=1;
+                                                        n--;
+                                                                    }
+                                                                            }
+                                                                                    if(n<=0){
+                                                                                                return true;}                                    return false;
+        }
+    
+};
